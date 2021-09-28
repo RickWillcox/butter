50ff59f4972183151c62a438ee4e17092aa297d8
Rick <rickwillcox90@gmail.com>
2021-09-28 22:42:32 +1000

    fixed client, joystick issue, removed warnings
    also icons

--- Testing/2Servers.bat
+++
@@ -1,4 +0,0 @@
-start C:\Users\Rick\Desktop\"Multiplayer Tutorial"\MultiplayerGodot\Testing\AuthenticationServer.exe
-timeout 2
-start C:\Users\Rick\Desktop\"Multiplayer Tutorial"\MultiplayerGodot\Testing\GatewayServer.exe"
-timeout 1
--- Testing/3Servers.bat
+++
@@ -1,7 +0,0 @@
-start C:\Users\Rick\Desktop\"Multiplayer Tutorial"\MultiplayerGodot\Testing\AuthenticationServer.exe
-timeout 2
-start C:\Users\Rick\Desktop\"Multiplayer Tutorial"\MultiplayerGodot\Testing\GatewayServer.exe"
-timeout 1
-start C:\Users\Rick\Desktop\"Multiplayer Tutorial"\MultiplayerGodot\Testing\WorldServer.exe
-timeout 1
-
